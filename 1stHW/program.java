ffbad5ef46ede2685b4ff7c5a0d44d9c869c8f06
Ярослав Розлуцкий <papa7717@mail.ru>
2022-09-21 12:25:34 +0300

    Доделал все мозможные пути получения числа в первой задаче, добави правки в програму

--- 1stHW/program.java
+++ 1stHW/program.java
@@ -1,8 +1,11 @@
 public class program {
 
-    static int[] solve(int a, int b, int c, int d) {
-      int[] arr = new int[b + 1];
+  static int[] solve2(int a, int b, int c, int d, int[] arr) {
+    if (a == b && arr[a] == 0) {
       arr[a] = 1;
+      return arr;
+    } else {
+      arr = solve2(a, b - 1, c, d, arr);
       for (int index = a + 1; index <= b; index++) {
         if (index % d == 0) {
           arr[index] = arr[index - c] + arr[index / d];
@@ -10,25 +13,9 @@ public class program {
           arr[index] = arr[index - c];
         }
       }
-      return arr;
     }
-
-    static int[] solve2(int a, int b, int c, int d, int[] arr) {
-      if (a == b && arr[a] == 0) {
-        arr[a] = 1;
-        return arr;
-      } else {
-        arr = solve2(a, b - 1, c, d, arr);
-        for (int index = a + 1; index <= b; index++) {
-          if (index % d == 0) {
-            arr[index] = arr[index - c] + arr[index / d];
-          } else {
-            arr[index] = arr[index - c];
-          }
-        }
-      }
-    return arr;
-    }
+  return arr;
+  }
   
     public static void main(String[] args) {
       int a = 6;
